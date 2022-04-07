12d0ed9a506f8fcefd2ba9f6b8aed5529a4530ab
v-asoliev <Abdukodir_Soliev@epam.com>
2022-04-07 16:58:58 +0500

    feat: add Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Task1
+﻿using System;
+
+namespace Task1
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            // TODO: Implement the task here.
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter some text: ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine($"first character: '{input[0]}'");
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    string msg = ex.Message;
+                    //Console.WriteLine(ex.Message);
+                    var defaultForegroundColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Empty string!");
+                    Console.ForegroundColor = defaultForegroundColor;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
